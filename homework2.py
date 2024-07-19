f6e060d4755fe6abf1bf6f0334116803a6569c2e
VladaPolonskaya <vlada-prim@mail.ru>
2024-07-19 19:00:45 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = 1.5/12
+print('Курс:',course_name,',','всего задач:',completed_tasks,'','затрачено часов:',hours_spent,',''среднее время выполнения:',time_for_one_task,'часа.')
